b56eadde18c98b7f53e4dc2feff00bef709b0dc7
phamthang44 <Ducthang4342@gmail.com>
2025-11-27 11:41:40 +0700

    adjust timeout of axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 5000,
+  timeout: 30000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
